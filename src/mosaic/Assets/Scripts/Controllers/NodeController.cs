e6ee18013c787115f58e4d238fb82cec01d16b3c
Jack Davis <mr.jackdavis@gmail.com>
2014-06-01 16:45:41 +1000

    Add repository interface

--- src/mosaic/Assets/Scripts/Controllers/NodeController.cs
+++ src/mosaic/Assets/Scripts/Controllers/NodeController.cs
@@ -1,13 +1,41 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
 using Mosaic.Models;
+using Mosaic.Infrastructure;
 
 namespace Mosaic.Controllers {
-	public class NodeController {
+	public class NodeController : IRepository<Node>{
+
 		public void Render(Node Node){
 			GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
 			cube.transform.position = Node.Position;
 			cube.transform.RotateAround (new Vector3 (0, 0, 0), new Vector3 (0.5F, 0.5F, 0), 20.2F);
 		}
+
+		#region IRepository implementation
+
+		public IEnumerable<Node> GetAll ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void Add ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void Remove ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void Update ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		#endregion
 	}
 }
---
+++ src/mosaic/Assets/Scripts/Infrastructure/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Mosaic.Infrastructure {
+	public interface IRepository<T> {
+		IEnumerable<T> GetAll ();
+		void Add ();
+		void Remove ();
+		void Update ();
+	}
+}
