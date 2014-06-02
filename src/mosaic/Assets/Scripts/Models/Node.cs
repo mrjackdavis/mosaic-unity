4d3d0a59535d8124234fcad3759d236e15e85f98
Jack Davis <mr.jackdavis@gmail.com>
2014-06-02 10:09:10 +1000

    Add orbit infrastructure

--- src/mosaic/Assets/Scripts/Models/Node.cs
+++ src/mosaic/Assets/Scripts/Models/Node.cs
@@ -4,5 +4,6 @@ using System.Collections;
 namespace Mosaic.Models {
 	public class Node {
 		public Vector3 Position { get; set; }
+		public float OrbitDistance {get;set;}
 	}
 }
--- src/mosaic/Assets/Scripts/MosaicManager.cs
+++ src/mosaic/Assets/Scripts/MosaicManager.cs
@@ -7,24 +7,38 @@ using Mosaic.Infrastructure;
 namespace Mosaic {
 	public class MosaicManager : MonoBehaviour {
 		private NodeController nodeController;
+		private GameObject Player;
 
 		// Use this for initialization
 		void Start () {
+			this.Player = this.gameObject;
+
 			IRenderer<Node> nodeRenderer = new NodeRenderer ();
 			IRepository<Node> nodeRepository = new MemoryRepository<Node> ();
 
 			nodeController = new NodeController (nodeRepository,nodeRenderer);
 
-			int mockNodes = 100;
+			int mockNodes = 300;
+
+			Node CurrentNode = null;
 
 			for(int i = 0; i+1 <= mockNodes; i++){
 				Node node = new Node ();
 				node.Position = new Vector3 (i*i%29, i*i%13, i*i%67);
+				node.OrbitDistance = 1F;
 				nodeController.AddNode (node);
+
+				if(i == mockNodes/2)
+					CurrentNode = node;
 			}
+
+			if (CurrentNode == null)
+				throw new UnityException ("Expected current node to not be null");
 
 
 			nodeController.RenderNodes ();
+
+			//Player.transform.position = new Vector3(CurrentNode.Position.x+CurrentNode.OrbitDistance,CurrentNode.Position.y,CurrentNode.Position.z);
 		}
 		
 		// Update is called once per frame
