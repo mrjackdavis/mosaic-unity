22cc9b255e3c598198cc6fa3131bfa0614b4a3e8
Jack Davis <mr.jackdavis@gmail.com>
2014-06-02 10:07:58 +1000

    Refactor sensitivity

--- src/mosaic/Assets/Scripts/PlayerManager.cs
+++ src/mosaic/Assets/Scripts/PlayerManager.cs
@@ -6,8 +6,7 @@ namespace Mosaic{
 	public class PlayerManager : MonoBehaviour {
 		
 		public float speed = 10.0F;
-		public float horizontalSpeed = 2.0F;
-		public float verticalSpeed = 2.0F;
+		public float mouseSensitivity = 4.0F;
 
 		// Use this for initialization
 		void Start () {
@@ -16,13 +15,13 @@ namespace Mosaic{
 		
 		// Update is called once per frame
 		void Update() {
-			float h = horizontalSpeed * Input.GetAxis("Mouse X");
-			float v = verticalSpeed * Input.GetAxis("Mouse Y");
+			float h = mouseSensitivity * Input.GetAxis("Mouse X");
+			float v = mouseSensitivity * Input.GetAxis("Mouse Y");
 
 			float translation = Input.GetAxis("Vertical") * speed;
 			translation *= Time.deltaTime;
 			transform.Translate(0, 0, translation);
-			transform.Rotate(v, h, 0);
+			transform.Rotate(-v, h, 0);
 		}
 	}
 }
